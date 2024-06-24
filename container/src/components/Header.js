c4fb64b2e4f7042058c44608b0d6129da45cd42e
preetham-p-m <preethampm1@gmail.com>
2024-06-24 19:19:26 +0530

    cleanup

--- container/src/components/Header.js
+++ container/src/components/Header.js
@@ -64,35 +64,33 @@ export default function Header({ isSignedIn, onSignOut }) {
   };
 
   return (
-    <React.Fragment>
-      <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        className={classes.appBar}
-      >
-        <Toolbar className={classes.toolbar}>
-          <Typography
-            variant="h6"
-            color="inherit"
-            noWrap
-            component={RouterLink}
-            to="/"
-          >
-            App
-          </Typography>
-          <Button
-            color="primary"
-            variant="outlined"
-            className={classes.link}
-            component={RouterLink}
-            to={isSignedIn ? "/" : "/auth/signin"}
-            onClick={onClick}
-          >
-            {isSignedIn ? "Logout" : "Login"}
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </React.Fragment>
+    <AppBar
+      position="static"
+      color="default"
+      elevation={0}
+      className={classes.appBar}
+    >
+      <Toolbar className={classes.toolbar}>
+        <Typography
+          variant="h6"
+          color="inherit"
+          noWrap
+          component={RouterLink}
+          to="/"
+        >
+          App
+        </Typography>
+        <Button
+          color="primary"
+          variant="outlined"
+          className={classes.link}
+          component={RouterLink}
+          to={isSignedIn ? "/" : "/auth/signin"}
+          onClick={onClick}
+        >
+          {isSignedIn ? "Logout" : "Login"}
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 }
